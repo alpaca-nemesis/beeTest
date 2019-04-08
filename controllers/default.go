94a664406c6abbaad152bb4c49ff3df88c263962
crowix <mdzhangkailai@126.com>
2019-04-08 21:06:03 +0800

    first time init

---
+++ controllers/default.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+
+/*************  INDEX  ******************/
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+func (c *MainController) Post() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+
+/*************  SEARCH  ******************/
+type SearchController struct {
+	beego.Controller
+}
+
+func (c *SearchController) Get() {
+	c.TplName = "index.html"
+}
+
+func (c *SearchController) Post() {
+	jsoninfo := c.GetString("content")
+	c.Data["text"] = jsoninfo
+	c.TplName = "index.html"
+}
---
+++ routers/router.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"beeTest/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/index", &controllers.MainController{})
+	beego.Router("/search", &controllers.SearchController{})
+}
+
+

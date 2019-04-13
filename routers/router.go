7b6d07e4fdbcc8694f329a97e27970b7e4583cd9
crowix <mdzhangkailai@126.com>
2019-04-13 23:11:07 +0800

    create a new model

--- routers/router.go
+++ routers/router.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.IndexController{})
 	beego.Router("/index", &controllers.IndexController{})
 	beego.Router("/search", &controllers.SearchController{})
 	beego.Router("/login", &controllers.LoginController{})
